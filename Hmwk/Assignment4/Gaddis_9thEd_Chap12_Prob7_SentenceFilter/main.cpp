3a9d9fd41eca5b93505cde863033d7270adba72c
Andrew G <guzmanandrewe@gmail.com>
2022-10-10 13:51:16 -0700

    Add files from 10/5 lecture, starter files for hmwk 4 and lab 3

---
+++ Hmwk/Assignment4/Gaddis_9thEd_Chap12_Prob7_SentenceFilter/main.cpp
@@ -0,0 +1,40 @@
+/* 
+ * File:   main.cpp
+ * Author: Andrew Guzman
+ * Created: September 30, 2022 @ 11:17 PM
+ * Purpose: Reads contents for first file, changes all letters to lowercase
+ *          and stores updated contents in a second file
+ */
+
+//System Libraries
+#include <iostream>  //Input/output Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Physics/Chemistry/Math/Conversion Higher Dimension Only
+
+//Structure Definitions
+
+//Function Prototypes
+
+//Program Execution Begins Here!!!
+
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+
+    //Declare Variables
+
+    //Initial Variables
+
+    //Map the Inputs to the Outputs
+
+    //Display the Inputs and Outputs
+
+    //Clean up the dynamic stuff
+
+    //Exit the code
+    return 0;
+}
+
---
+++ Hmwk/Assignment4/Gaddis_9thEd_Chap12_Prob8_ArrayFileFunctions/main.cpp
@@ -0,0 +1,39 @@
+/* 
+ * File:   main.cpp
+ * Author: Andrew Guzman
+ * Created: September 30, 2022 @ 11:17 PM
+ * Purpose: Reads data from file into an array and display array's contents.
+ */
+
+//System Libraries
+#include <iostream>  //Input/output Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Physics/Chemistry/Math/Conversion Higher Dimension Only
+
+//Structure Definitions
+
+//Function Prototypes
+
+//Program Execution Begins Here!!!
+
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+
+    //Declare Variables
+
+    //Initial Variables
+
+    //Map the Inputs to the Outputs
+
+    //Display the Inputs and Outputs
+
+    //Clean up the dynamic stuff
+
+    //Exit the code
+    return 0;
+}
+
